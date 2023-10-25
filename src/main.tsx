18333987caec15bfe9a55d3d3537dd4418df1d47
huynguyen52 <61266768+huynguyen52@users.noreply.github.com>
2023-10-25 21:53:30 +0700

    initial project

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { ApolloProvider } from '@apollo/client';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import client from './client';
+import './index.css';
+import theme from './theme';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <ApolloProvider client={client}>
+        <CssBaseline />
+        <App />
+      </ApolloProvider>
+    </ThemeProvider>
+  </React.StrictMode>,
+);
---
+++ src/theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  components: {},
+  typography: {
+    fontFamily: ['Noto Sans', 'sans-serif'].join(','),
+    htmlFontSize: 10,
+  },
+  palette: {},
+});
+
+export default theme;
