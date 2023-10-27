eb1150b393f955a3e7c5ae489612488e8e4d45a8
huynguyen52 <61266768+huynguyen52@users.noreply.github.com>
2023-10-28 01:26:32 +0700

    - implemen banner - setup slider auto pagination

--- src/pages/client/home/section/Banner.tsx
+++ src/pages/client/home/section/Banner.tsx
@@ -1,5 +1,70 @@
 import { Box } from '@mui/system';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import banner1 from '@/assets/banner1.png';
+import banner2 from '@/assets/banner2.jpeg';
+import banner3 from '@/assets/banner3.jpeg';
+
+interface BoxWithBg {
+  url: string;
+}
+
+const imageList = [banner1, banner2, banner3];
+
+const BoxWithBg = (props: BoxWithBg) => {
+  const { url } = props;
+  return (
+    <Box
+      sx={{
+        height: '90vh',
+        backgroundImage: `url(${url})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+      }}
+    />
+  );
+};
 
 export const Banner = () => {
-  return <Box>Banner</Box>;
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index: number, className: string) {
+      return `<div class="${className}">
+        <img src="${imageList[index]}"} />
+      </div>`;
+    },
+  };
+  return (
+    <Box
+      sx={{
+        '& .swiper-pagination-bullet': {
+          width: '40px',
+          height: '40px',
+          borderRadius: '50%',
+          overflow: 'hidden',
+          border: '2px solid #fff',
+          opacity: 0.6,
+        },
+        '& .swiper-pagination-bullet-active': {
+          opacity: 1,
+        },
+        '& .swiper-pagination-bullet img': {
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+        },
+      }}
+    >
+      <Swiper pagination={pagination} modules={[Pagination]} className='mySwiper'>
+        {imageList.map((img, index) => (
+          <SwiperSlide key={index}>
+            <BoxWithBg url={img} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
+  );
 };
--- src/pages/client/home/section/CustomSlider.tsx
+++ src/pages/client/home/section/CustomSlider.tsx
@@ -1,5 +1,44 @@
+import { Button } from '@mui/material';
 import { Box } from '@mui/system';
+import { useState } from 'react';
+import type { Swiper as SwiperType } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 export const CustomSlider = () => {
-  return <Box>CustomSlider</Box>;
+  const [swiper, setSwiper] = useState<SwiperType | null>(null);
+  const slideTo = (index: number) => {
+    if (swiper) {
+      console.log(swiper);
+      swiper.slideTo(index);
+    }
+  };
+
+  return (
+    <Box>
+      <Button onClick={() => slideTo(0)}>Click</Button>
+      <Swiper
+        onSwiper={setSwiper}
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className='mySwiper'
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    </Box>
+  );
 };
