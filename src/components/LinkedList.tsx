a54e152f6f869e4e5b9a0fc0c9973867df500cc4
huynguyen52 <61266768+huynguyen52@users.noreply.github.com>
2023-10-31 17:41:01 +0700

    Finish linked list

--- src/components/LinkedList.tsx
+++ src/components/LinkedList.tsx
@@ -1,29 +1,32 @@
-import { Container, Typography } from '@mui/material';
+import { Container, useMediaQuery } from '@mui/material';
 import { Box } from '@mui/system';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import { Title } from '@/components/Title';
 import banner1 from '@/assets/website-1.png';
 import banner2 from '@/assets/website-2.jpeg';
 import banner3 from '@/assets/website-3.png';
 import banner4 from '@/assets/website-4.png';
 import banner5 from '@/assets/website-5.png';
 import banner6 from '@/assets/website-6.png';
+import mountain from '@/assets/mountain.svg';
 
 const lists = [banner1, banner2, banner3, banner4, banner5, banner6];
 
 export const LinkedList = () => {
+  const mobile = useMediaQuery('(max-width:600px)');
+
   return (
-    <Box>
+    <Box sx={{ backgroundImage: `url(${mountain})` }}>
       <Container maxWidth='xl'>
-        <Typography>LinkedList</Typography>
-        <Box>
+        <Title title='Liên kết website' sub='Ẩm thực' align='center' />
+        <Box sx={{ mt: ['50px', '70px', '100px'], pb: ['10px', '30px', '50px'] }}>
           <Swiper
-            slidesPerView={6}
+            slidesPerView={mobile ? 3 : 5}
             spaceBetween={30}
-            centeredSlides={true}
             autoplay={{
               delay: 2500,
               disableOnInteraction: false,
@@ -31,13 +34,14 @@ export const LinkedList = () => {
             pagination={{
               clickable: true,
             }}
+            loop={true}
             navigation={true}
             modules={[Autoplay, Pagination, Navigation]}
             className='mySwiper'
           >
             {lists.map((item, index) => (
               <SwiperSlide key={index}>
-                <Box sx={{ width: '160px', height: '160px' }}>
+                <Box width={{ lg: '160px', md: '120px', xs: '80px' }} height={{ lg: '160px', md: '120px', xs: '80px' }}>
                   <Box component='img' sx={{ width: '100%', objectFit: 'cover' }} src={item} />
                 </Box>
               </SwiperSlide>
--- src/pages/client/home/index.tsx
+++ src/pages/client/home/index.tsx
@@ -12,7 +12,6 @@ export const Home = () => {
       <OutstadingImage />
       <LinkedList />
       <OutstandingBlogs />
-
       <Footer />
     </Box>
   );
