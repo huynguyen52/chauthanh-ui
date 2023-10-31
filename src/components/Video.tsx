31e9cf48ad1a9f170f54f8c5952622843d4dfd5f
huynguyen52 <61266768+huynguyen52@users.noreply.github.com>
2023-10-31 21:59:03 +0700

    Video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,10 +1,25 @@
 import { Container } from '@mui/material';
 import { Box } from '@mui/system';
+import logoLeft from '@/assets/logo_left.svg';
+import logoRight from '@/assets/logo_right.svg';
 
 export const Video = () => {
   return (
     <Box>
-      <Container maxWidth='xl'>Video</Container>
+      <Container maxWidth='xl'>
+        <Box my='96px' position='relative'>
+          <Box zIndex={-1} top={0} left={-200} component='img' src={logoLeft} position='absolute' />
+          <Box zIndex={-1} bottom={0} right={-200} component='img' src={logoRight} position='absolute' />
+          <iframe
+            width='100%'
+            height='500px'
+            src='https://www.youtube.com/embed/EibeiQH0T7c?si=WJHxqLvfpcGymBJg'
+            title='YouTube video player'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+            allowFullScreen
+          ></iframe>
+        </Box>
+      </Container>
     </Box>
   );
 };
